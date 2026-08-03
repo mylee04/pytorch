fbd3650cedc9cb45449068f2a2f65442b3068f7f
Rob Timpe <rtimpe@openteams.com>
2026-08-03 23:21:43 +0000

    [dynamo, 3.15] Update error messages
    ghstack-source-id: dfcafbab46fe7a4dd99c4e68bc13ca43e674809c
    Pull-Request: https://github.com/pytorch/pytorch/pull/191814

--- test/dynamo/test_getitem.py
+++ test/dynamo/test_getitem.py
@@ -1123,10 +1123,11 @@ class GetItemTests(torch._dynamo.test_case.TestCase):
             try:
                 list(IterNoNext())
             except TypeError as exc:
-                return str(exc) == "iter() returned non-iterator of type 'IterNoNext'"
-            return False
+                return str(exc)
 
-        self.assertEqual(fn(), self._compile(fn))
+        out = self._compile(fn)
+        self.assertTrue(type(out) is str)
+        self.assertEqual(fn(), out)
 
     def test_list_constructor_rejects_keywords(self):
         def fn():
@@ -1195,10 +1196,12 @@ class GetItemTests(torch._dynamo.test_case.TestCase):
             try:
                 tuple(IterNoNext())
             except TypeError as exc:
-                return str(exc) == "iter() returned non-iterator of type 'IterNoNext'"
+                return str(exc)
             return False
 
-        self.assertEqual(fn(), self._compile(fn))
+        out = self._compile(fn)
+        self.assertTrue(type(out) is str)
+        self.assertEqual(fn(), out)
 
     def test_tuple_constructor_rejects_keywords(self):
         def fn():
